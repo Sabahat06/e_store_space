2650db745d71697d0278609c84632e1d15d23c73
Muhammad Bilal <bilalroze94@gmail.com>
2022-01-20 23:38:00 +0500

    Almost All API Integrated

--- lib/view/home screen and tab seller/buyer_screen_tab_seller.dart
+++ lib/view/home screen and tab seller/buyer_screen_tab_seller.dart
@@ -1,17 +1,8 @@
 import 'package:e_store_space/controller/bottom_bar_controller.dart';
-import 'package:e_store_space/widgets/AlertDialogeWidget.dart';
-import 'package:e_store_space/widgets/my_filled_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:e_store_space/controller/cart/shop_cart_screen_controller.dart';
-import 'package:e_store_space/controller/cart_controller.dart';
-import 'package:e_store_space/controller/page_controller.dart';
-import 'package:e_store_space/models/sub_category_model.dart';
-import 'package:e_store_space/settings/color_palates.dart';
-import 'package:e_store_space/view/product/product_screen.dart';
-import 'package:e_store_space/widgets/my_appbar.dart';
 
 class BuyerScreenTabSeller extends StatelessWidget {
   BottomBarController bottomBarController = Get.find();
@@ -46,7 +37,7 @@ class BuyerScreenTabSeller extends StatelessWidget {
       padding: const EdgeInsets.only(left: 12.0, right: 12, top: 10),
       child: Card(
         color: Colors.white,
-        elevation: 10,
+        elevation: 3,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
         ),
@@ -66,41 +57,16 @@ class BuyerScreenTabSeller extends StatelessWidget {
                 )
               ),
             ),
-            SizedBox(width: 10.w,),
+            SizedBox(width: 20.w,),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 2.h,),
-                Text('Mobile Phone', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.sp, color: Colors.black),),
                 SizedBox(height: 3.h,),
-                Text('Product Quantity : 2', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.sp, color: Colors.black),),
-                SizedBox(height: 3.h,),
-                Text('40\$', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.sp, color: Colors.black),),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SizedBox(width: 45.w,),
-                    MyFilledButton(
-                      width: 80,
-                      height: 30,
-                      color: Colors.red,
-                      borderRadius: 10,
-                      fontSize: 16.sp,
-                      txt: "Cancel",
-                      ontap: (){},
-                    ),
-                    SizedBox(width: 15.w,),
-                    MyFilledButton(
-                      width: 80,
-                      height: 30,
-                      color: Colors.blue,
-                      borderRadius: 10,
-                      fontSize: 16.sp,
-                      txt: "Accept",
-                      ontap: (){},
-                    ),
-                  ],
-                ),
+                Text('Mobile Phone', style: TextStyle(fontSize: 15.sp, color: Colors.black),),
+                SizedBox(height: 5.h,),
+                Text('Product Quantity : 2', style: TextStyle(fontSize: 15.sp, color: Colors.black),),
+                SizedBox(height: 5.h,),
+                Text('40\$', style: TextStyle(fontSize: 15.sp, color: Colors.black),),
               ],
             ),
           ],
